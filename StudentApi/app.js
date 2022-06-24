6f20101aa066e05f04cff41af9b0425a70abd7f5
Meerasaheb Moka <m.moka@zenasr.com>
2022-06-24 11:27:43 +0530

    adding the student management base Api

---
+++ StudentApi/app.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const http=require("http");
+const mongoose=require("mongoose");
+const cors=require("cors");
+
+
+const app=express();
+const server=http.createServer(app);
+const PORT=process.env.PORT || 3001;
+
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/students",(req,res)=>{
+    res.send("hello-users");
+})
+
+
+mongoose
+  .connect("mongodb+srv://meerasaheb:MM.Zensar230@meerasaheb.7ajzpjx.mongodb.net/students?retryWrites=true&w=majority")
+  .then((res) => console.log(`Mongoose connected to db successfully ${res}`))
+  .catch((err) =>
+    console.error(`Mongoose connection to db failed ${err.message}`)
+  );
+
+
+  server.listen(PORT, () => {
+    console.log(`Express application is listening at http://localhost:${PORT}`);
+  });
